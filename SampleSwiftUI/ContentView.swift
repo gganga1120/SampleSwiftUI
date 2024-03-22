f2321c9ee161702955df2e1775f68dd5457030e7
Ganga Ganapathi Gajulapati <59639073+ggajulapati@users.noreply.github.com>
2024-03-22 18:36:42 +0530

    initial commit

---
+++ SampleSwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SampleSwiftUI
+//
+//  Created by ggajulapati on 22/03/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SampleSwiftUI/SampleSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleSwiftUIApp.swift
+//  SampleSwiftUI
+//
+//  Created by ggajulapati on 22/03/24.
+//
+
+import SwiftUI
+
+@main
+struct SampleSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
